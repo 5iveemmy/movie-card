03f7bc885fa3ee1ac4098812a3f33d51bdb7a326
5iveemmy <ainemmanuel87@gmail.com>
2021-09-05 01:17:17 +0100

    add api

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { BsSearch, MdPlaylistPlay, FiHeart, IoMdPhotos } from "react-icons/all";
 import "./Platform.css";
+import axios from "axios";
 
 function Platform() {
+  const options = {
+    method: "GET",
+    url: "https://data-imdb1.p.rapidapi.com/genres/",
+    headers: {
+      "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
+      "x-rapidapi-key": "c64538c0e3msh7c9379f641df305p18061djsncb2135f14de5",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  console.log(options);
+
   return (
     <div className="platform">
       <div className="platform-wrap">
